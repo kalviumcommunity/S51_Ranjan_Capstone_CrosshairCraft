8dbd402448cdb50f42f386c5dbb9e6c4c62ad2b2
M-Ranjan <ranjan.m1325@gmail.com>
2024-03-15 11:01:53 +0530

    Created frontend page for Loginpages

--- FrontEnd/CrosshairCraft/src/App.jsx
+++ FrontEnd/CrosshairCraft/src/App.jsx
@@ -1,21 +1,26 @@
 import React from 'react'
-import {ScaleLoader} from 'react-spinners'
-
-
-
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import './App.css'
-
+import LoginPage from './components/LoginPage'
+import Home from './components/Home'
+import SignupPage from './components/SignupPage'
 
 function App() {
- 
-
-  return (
+   return (
     <>
       <h1>Crosshair Craft</h1> 
       <p>Welcome to CrosshairCraft, the ultimate crosshair generator for FPS enthusiasts. This tool empowers you to design and customize your crosshair, offering a personalized aiming experience tailored to your preferences. Whether you are a seasoned gamer or a casual player, CrosshairCraft makes it easy to elevate your aiming precision with style.</p><br />
-      <div className="loader">
-        <ScaleLoader size = {20} color='white'></ScaleLoader>
+       <div>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<Home/>} />
+            <Route path='/loginpage' element={<LoginPage/>} />
+            <Route path='/signuppage' element={<SignupPage/>} />          
+          </Routes>
+        </BrowserRouter>
       </div>
+
+
     </>
   )
 }
