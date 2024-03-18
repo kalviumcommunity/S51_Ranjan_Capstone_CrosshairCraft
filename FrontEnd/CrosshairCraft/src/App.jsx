c0f3ff6c0b57489c26e8d169e555518cbc2e1e02
M-Ranjan <ranjan.m1325@gmail.com>
2024-03-18 10:57:20 +0530

    Created NavBar

--- FrontEnd/CrosshairCraft/src/App.jsx
+++ FrontEnd/CrosshairCraft/src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import './App.css'
 import LoginPage from './components/LoginPage'
+import NavBar from './components/NavBar'
 import Home from './components/Home'
 import SignupPage from './components/SignupPage'
 
 function App() {
    return (
     <>
+      <NavBar/><br />
       <h1>Crosshair Craft</h1> 
       <p>Welcome to CrosshairCraft, the ultimate crosshair generator for FPS enthusiasts. This tool empowers you to design and customize your crosshair, offering a personalized aiming experience tailored to your preferences. Whether you are a seasoned gamer or a casual player, CrosshairCraft makes it easy to elevate your aiming precision with style.</p><br />
        <div>
@@ -15,7 +17,7 @@ function App() {
           <Routes>
             <Route path='/' element={<Home/>} />
             <Route path='/loginpage' element={<LoginPage/>} />
-            <Route path='/signuppage' element={<SignupPage/>} />          
+            <Route path='/signuppage' element={<SignupPage/>} /> 
           </Routes>
         </BrowserRouter>
       </div>
