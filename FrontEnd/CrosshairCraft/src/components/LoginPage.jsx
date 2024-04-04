b4beee1a8131ca8de65f9cc9e11e97f618e827bb
M-Ranjan <ranjan.m1325@gmail.com>
2024-04-04 12:40:45 +0530

    Created googleauth component

--- FrontEnd/CrosshairCraft/src/components/LoginPage.jsx
+++ FrontEnd/CrosshairCraft/src/components/LoginPage.jsx
@@ -112,6 +112,7 @@ function Login() {
             <button className="buttonsubmit" type="submit">
             Login
             </button>
+
             <div id="signinbtn"></div>
         </div>
         
