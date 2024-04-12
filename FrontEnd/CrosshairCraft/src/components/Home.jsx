7ed1305d9fa109e6b4a2a2cc13ebbfbe86c1d53d
M-Ranjan <ranjan.m1325@gmail.com>
2024-04-12 13:52:57 +0530

    Created search bar in navbar component

--- FrontEnd/CrosshairCraft/src/components/Home.jsx
+++ FrontEnd/CrosshairCraft/src/components/Home.jsx
@@ -31,6 +31,7 @@ function Home() {
         <>
             <NavBar/>
             <div className='Home'>
+                
                 <h1>Crosshair Craft</h1><br /> 
                 <p>Welcome to CrosshairCraft, the ultimate crosshair generator for FPS enthusiasts. This tool empowers you to design and customize your crosshair, offering a personalized aiming experience tailored to your preferences. Whether you are a seasoned gamer or a casual player, CrosshairCraft makes it easy to elevate your aiming precision with style.</p><br />
             </div>
