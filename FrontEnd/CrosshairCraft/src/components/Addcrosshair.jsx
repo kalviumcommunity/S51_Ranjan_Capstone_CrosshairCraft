9908f23c21dfc08ad9441eb2e8aa2a54157a2ac8
M-Ranjan <ranjan.m1325@gmail.com>
2024-04-29 15:11:40 +0530

    Added styles

--- FrontEnd/CrosshairCraft/src/components/Addcrosshair.jsx
+++ FrontEnd/CrosshairCraft/src/components/Addcrosshair.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import axios from "axios";
+import NavBar from "./NavBar";
 import "../App.css";
 
 function Addcrosshair() {
@@ -43,58 +44,69 @@ function Addcrosshair() {
   };
 
   return (
-    <div>
-      <h2>Add Crosshair</h2>
-      {error && <div>{error}</div>}
-      <form onSubmit={handlesubmit}>
-        <label>
-          Crosshair ID:
-          <input
-            type="text"
-            name="CrosshairID"
-            value={formdata.CrosshairID}
-            onChange={handlechange}
-          />
-        </label>
-        <label>
-          Color:
-          <input
-            type="text"
-            name="Color"
-            value={formdata.Color}
-            onChange={handlechange}
-          />
-        </label>
-        <label>
-          Type:
-          <input
-            type="text"
-            name="Type"
-            value={formdata.Type}
-            onChange={handlechange}
-          />
-        </label>
-        <label>
-          Game:
-          <input
-            type="text"
-            name="Game"
-            value={formdata.Game}
-            onChange={handlechange}
-          />
-        </label>
-        <label>
-          Created By:
-          <input
-            type="text"
-            name="CreatedBy"
-            value={formdata.CreatedBy}
-            onChange={handlechange}
-          />
-        </label>
-        <button type="submit">Submit</button>
-      </form>
-    </div>
+    <>
+      <NavBar />
+      <div className="form-container">
+        <h2>Add Crosshair</h2>
+        <br />
+        {error && <div className="error-message">{error}</div>}
+        <form onSubmit={handlesubmit}>
+          <div className="input-field">
+            <label htmlFor="crosshair-id">ID</label>
+            <input
+              type="text"
+              id="crosshair-id"
+              name="CrosshairID"
+              value={formdata.CrosshairID}
+              onChange={handlechange}
+            />
+          </div>
+          <div className="input-field">
+            <label htmlFor="color">Color</label>
+            <input
+              type="text"
+              id="color"
+              name="Color"
+              value={formdata.Color}
+              onChange={handlechange}
+            />
+          </div>
+          <div className="input-field">
+            <label htmlFor="type">Type</label>
+            <input
+              type="text"
+              id="type"
+              name="Type"
+              value={formdata.Type}
+              onChange={handlechange}
+            />
+          </div>
+          <div className="input-field">
+            <label htmlFor="game">Game</label>
+            <input
+              type="text"
+              id="game"
+              name="Game"
+              value={formdata.Game}
+              onChange={handlechange}
+            />
+          </div>
+          <div className="input-field">
+            <label htmlFor="created-by">CreatedBy</label>
+            <input
+              type="text"
+              id="created-by"
+              name="CreatedBy"
+              value={formdata.CreatedBy}
+              onChange={handlechange}
+            />
+          </div>
+          <button type="submit" className="submit-button">
+            Submit
+          </button>
+        </form>
+      </div>
+    </>
   );
 }
 
